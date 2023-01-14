5eb6e16fdc06b04be5a6ed6e0ed10138c23944c0
ayu <chenurwahidaayu2000@gmail.com>
2023-01-14 21:42:16 +0800

    fixing navigation friend page

--- app/src/main/java/com/example/YouOweMeProject/FriendsList/Friend/SettleUpActivity.java
+++ app/src/main/java/com/example/YouOweMeProject/FriendsList/Friend/SettleUpActivity.java
@@ -1,4 +1,37 @@
 package com.example.YouOweMeProject.FriendsList.Friend;
 
-public class SettleUpActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.YouOweMeProject.FriendsList.Friend.SettleUp.Confirmation;
+import com.example.YouOweMeProject.FriendsList.FriendActivity;
+import com.example.YouOweMeProject.R;
+import com.example.YouOweMeProject.Settings.ProfileActivity;
+import com.example.YouOweMeProject.SettingsActivity;
+
+public class SettleUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+       
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friendslist_friend_settleup);
+        getSupportActionBar().setTitle("Settle Up");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button Settleup = findViewById(R.id.btnsettleup);
+        Settleup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettleUpActivity.this, Confirmation.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
 }
